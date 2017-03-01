f27908601755ccdd333e2705be3b2a663021d697
hhstore <hhstore@qq.com>
2017-03-01 14:48:09 +0800

    update - 网易云信-短信验证码服务 API 对接 SDK 实现, 包含使用示例, 以及 API 调用返回结果.

--- sms.py
+++ sms.py
@@ -207,7 +207,7 @@ if __name__ == '__main__':
 """
 # 发送验证码短信:
 url: https://api.netease.im/sms/sendcode.action
-HTTP-header: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'CurTime': '1488347303', 'CheckSum': 'daf9bf5bf2d68715c9c12d5612b370ff27623273', 'Nonce': '775dd54709e74a808202a318fbb453eb', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4d0'}
+HTTP-header: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'CurTime': '1488347303', 'CheckSum': 'daf9bf5bf2d68715c9c12d5612b370ff27623273', 'Nonce': '775dd54709e74a808202a318fbb453eb', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4XX'}
 
 HTTP-data: {'mobile': '13380789XXX'}
 	status: 200 	result: b'{"code":200,"msg":"14","obj":"3367"}'
@@ -215,7 +215,7 @@ HTTP-data: {'mobile': '13380789XXX'}
 
 # 查询发送状态:
 url: https://api.netease.im/sms/querystatus.action
-HTTP-header: {'Nonce': '61e9df9723e54bbf9f14892915ee4406', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'CurTime': '1488347349', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4d0', 'CheckSum': 'e9d47dc5c3347d906de1dbe8f8261a93fe7da01d'}
+HTTP-header: {'Nonce': '61e9df9723e54bbf9f14892915ee4406', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'CurTime': '1488347349', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4XX', 'CheckSum': 'e9d47dc5c3347d906de1dbe8f8261a93fe7da01d'}
 
 HTTP-data: {'sendid': '14'}
 	status: 200 	result: b'{"code":200,"obj":[{"updatetime":1488347319329,"status":1,"mobile":"+86-13380789XXX"}]}'
@@ -223,7 +223,7 @@ HTTP-data: {'sendid': '14'}
 
 # 验证验证码是否正确:
 url: https://api.netease.im/sms/verifycode.action
-HTTP-header: {'Nonce': 'eb66cf06072647f5af66a8e7ef6dcba0', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'CurTime': '1488347350', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4d0', 'CheckSum': '205618d4dc2d152b6ba1b3a85dc855292991109f'}
+HTTP-header: {'Nonce': 'eb66cf06072647f5af66a8e7ef6dcba0', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'CurTime': '1488347350', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4XX', 'CheckSum': '205618d4dc2d152b6ba1b3a85dc855292991109f'}
 
 HTTP-data: {'mobile': '13380789XXX', 'code': '3367'}
 	status: 200 	result: b'{"code":200}'
@@ -234,14 +234,14 @@ HTTP-data: {'mobile': '13380789XXX', 'code': '3367'}
 # 发送模板验证码:
 		code:472451
 url: https://api.netease.im/sms/sendtemplate.action
-HTTP-header: {'CurTime': '1488347659', 'CheckSum': '371657b31fb74b94d91a155930f3b2f4d8ce0766', 'Nonce': '164d8355b7464bef93bfe31fefe927d7', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4d0'}
+HTTP-header: {'CurTime': '1488347659', 'CheckSum': '371657b31fb74b94d91a155930f3b2f4d8ce0766', 'Nonce': '164d8355b7464bef93bfe31fefe927d7', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4XX'}
 HTTP-data: {'mobiles': "['13380789XXX']", 'params': "['472451']", 'templateid': '3049132'}
 	status: 200 	result: b'{"code":200,"msg":"sendid","obj":15}'
 
 
 # 模板验证码, 状态校验:(支持查询状态)
 url: https://api.netease.im/sms/querystatus.action
-HTTP-header: {'CurTime': '1488347793', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4d0', 'CheckSum': 'a309b5ec0b978bc09ba4fd4de6487e5e3662231e', 'Nonce': '0a603bd67b434acab1b37ee14e9b7dc4'}
+HTTP-header: {'CurTime': '1488347793', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4XX', 'CheckSum': 'a309b5ec0b978bc09ba4fd4de6487e5e3662231e', 'Nonce': '0a603bd67b434acab1b37ee14e9b7dc4'}
 HTTP-data: {'sendid': '15'}
 	status: 200 	result: b'{"code":200,"obj":[{"updatetime":1488347672407,"status":1,"mobile":"13380789XXX"}]}'
 
@@ -250,7 +250,7 @@ HTTP-data: {'sendid': '15'}
 #   - 413	验证失败(短信服务)
 #
 url: https://api.netease.im/sms/verifycode.action
-HTTP-header: {'CurTime': '1488347794', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4d0', 'CheckSum': '981c8e6888676d67eaea0139e12890cb8984c3f4', 'Nonce': '87e511c76e1947008f684529ee3f02e7'}
+HTTP-header: {'CurTime': '1488347794', 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'AppKey': '9b2a9ade419055031a6e3fab8f89e4XX', 'CheckSum': '981c8e6888676d67eaea0139e12890cb8984c3f4', 'Nonce': '87e511c76e1947008f684529ee3f02e7'}
 HTTP-data: {'mobile': '13380789XXX', 'code': '472451'}
 	status: 200 	result: b'{"code":413,"msg":"verify err","obj":1}'
 
